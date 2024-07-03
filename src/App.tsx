47dd30c1961092c68628b03f53c79977f9002ea0
AndrichSanych <Prima_rq56@itstep.student.org>
2024-07-03 12:21:55 +0300

    My pizza app

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+//import LoginPage from "./components/login/LoginPage.tsx";
+import CategoriesPage from "./components/categories/CategoriesPage.tsx";
+
+function App() {
+
+  return (
+      <>
+        <CategoriesPage/>
+      </>
+  )
+}
+
+export default App
